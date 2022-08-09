8ca493a0891aab0b42950d1dbced4478f148b2c6
Marcos Albarado <malbarado@combustiblesdelnorte.com.ar>
2022-08-09 08:06:01 -0300

    CNLogistica

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import logo1 from './LOGO1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo1} className='App-logo'/>
+        <a> malbarado@hotmail.com </a>
+        <a> Tel: 3876317387 </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
